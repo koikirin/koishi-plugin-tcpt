8b0f16a547a14d3f98a6f380206abf6df98523db
Hieuzest <girkirin@hotmail.com>
2023-08-16 14:43:35 +0800

    style: lint

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,13 @@ async function query(ctx: Context, id?: number, name?: string, filters: object =
     const cursor = ctx.mahjong.database.db('tziakcha').collection('matches').find({ 'u.n': name }).sort('st', 'descending').limit(1)
     const doc = await cursor.next()
     if (doc) {
-      for (const u of doc.u) if (u.n == name) id = u.i
+      for (const u of doc.u) if (u.n === name) id = u.i
     } else return
   }
 
   const cursor = ctx.mahjong.database.db('tziakcha').collection('matches').find({ 'u.i': id, ...filters }).sort('st', 'descending')
 
-  let stats = {
+  const stats = {
     cnt: 0,
     cntr: 0,
     r1: 0,
@@ -37,7 +37,7 @@ async function query(ctx: Context, id?: number, name?: string, filters: object =
     chong: 0,
     btsumo: 0,
     cuohu: 0,
-    trend: ''
+    trend: '',
   }
 
   for await (let doc of cursor) {
@@ -54,17 +54,17 @@ async function query(ctx: Context, id?: number, name?: string, filters: object =
         stats[`r${r}s`] += u.s
         stats.trend += `${r}`
       }
-    });
+    })
 
     for (const rnd of doc.rounds) {
-      let { rs, rp } = rnd
+      const { rs, rp } = rnd
       if (rs[idx] > 0) {
         stats.hule += 1
         if (rs[(idx + 1) % 4] === rs[(idx + 2) % 4] && rs[(idx + 1) % 4] === rs[(idx + 3) % 4]) stats.tsumo += 1
-      } else if (rs[0] == 0 && rs[1] == 0 && rs[2] == 0 && rs[3] == 0) {
-
+      } else if (rs[0] === 0 && rs[1] === 0 && rs[2] === 0 && rs[3] === 0) {
+        ;
       } else if (rs[idx] < Math.max(...rs) && rs[idx] > Math.min(...rs)) {
-
+        ;
       } else if (rs[(idx + 1) % 4] > rs[idx % 4] && rs[(idx + 2) % 4] > rs[idx % 4] && rs[(idx + 3) % 4] > rs[idx % 4]) {
         stats.chong += 1
       } else {
@@ -75,19 +75,18 @@ async function query(ctx: Context, id?: number, name?: string, filters: object =
     }
   }
 
-  if (stats.hule == 0)
-    return '错误：账号胡率为0'
+  if (stats.hule === 0) { return '错误：账号胡率为0' }
 
   function p(num: number, style = 'percent'): string {
     return new Intl.NumberFormat('default', {
-      style: style,
+      style,
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
-    }).format(num);
+    }).format(num)
   }
 
   const scores = stats.r1s + stats.r2s + stats.r3s + stats.r4s - stats.rps
-  let msg = `${name} 合计${stats.cnt}战 [${stats.r1}/${stats.r2}/${stats.r3}/${stats.r4}]
+  const msg = `${name} 合计${stats.cnt}战 [${stats.r1}/${stats.r2}/${stats.r3}/${stats.r4}]
 和率${p(stats.hule / stats.cntr)} 铳率${p(stats.chong / stats.cntr)} 平分${p(scores / stats.cntr, 'decimal')}
 自摸率${p(stats.tsumo / stats.hule)} 被摸率${p(stats.btsumo / stats.cntr)} 错和率${p(stats.cuohu / stats.cntr)}
 最近战绩 [${stats.trend.slice(0, 10).split('').reverse().join('')}]`
@@ -100,37 +99,37 @@ async function queryNames(ctx: Context, id?: number, name?: string) {
     const cursor = ctx.mahjong.database.db('tziakcha').collection('matches').find({ 'u.n': name }).sort('st', 'descending').limit(1)
     const doc = await cursor.next()
     if (doc) {
-      for (const u of doc.u) if (u.n == name) id = u.i
+      for (const u of doc.u) if (u.n === name) id = u.i
     } else return
   }
 
   const cursor = ctx.mahjong.database.db('tziakcha').collection('matches').aggregate([
-    { "$match": { "u.i": id } },
+    { '$match': { 'u.i': id } },
     {
-      "$project": {
-        "list": {
-          "$filter": {
-            "input": "$u",
-            "as": "item",
-            "cond": {
-              "$eq": ["$$item.i", id]
-            }
-          }
-        }
-      }
+      '$project': {
+        'list': {
+          '$filter': {
+            'input': '$u',
+            'as': 'item',
+            'cond': {
+              '$eq': ['$$item.i', id],
+            },
+          },
+        },
+      },
     },
-    { "$unwind": "$list" },
+    { '$unwind': '$list' },
     {
-      "$group": {
-        "_id": "$list.n",
-        "count": {
-          "$count": {}
-        }
-      }
-    }
+      '$group': {
+        '_id': '$list.n',
+        'count': {
+          '$count': {},
+        },
+      },
+    },
   ])
 
-  let names: { [key: string]: number } = {}
+  const names: { [key: string]: number } = {}
   for await (const doc of cursor) {
     names[doc._id] = doc.count
   }
@@ -147,7 +146,7 @@ export function apply(ctx: Context) {
         'g.n': 16,
         'g.l': 8,
         'g.b': 8,
-        'rd.15': { $exists: true }
+        'rd.15': { $exists: true },
       }
       let extra = '\n*仅计入完场全庄'
       if (options.all) {
@@ -156,7 +155,7 @@ export function apply(ctx: Context) {
       } else if (options.common) {
         filters = {
           'g.l': 8,
-          'g.b': 8
+          'g.b': 8,
         }
         extra = '\n*仅计入8(8)'
       }
--- src/utils.ts
+++ src/utils.ts
@@ -1,69 +1,68 @@
-function calc_rk(mp){
-  var rks=[0,0,0,0];
-  var i,j;
-  for(i=0;i<4;++i){
-    for(j=0;j<4;++j){
-      if(i==j)continue;
-      if(mp[i]<mp[j])++rks[i];
-    }
-  }
-  return rks;
-}
+// function calcRK(mp) {
+//   const rks = [0, 0, 0, 0]
+//   let i, j
+//   for (i = 0; i < 4; ++i) {
+//     for (j = 0; j < 4; ++j) {
+//       if (i == j) continue
+//       if (mp[i] < mp[j])++rks[i]
+//     }
+//   }
+//   return rks
+// }
 
-function calc_rs(v,b){
-  var a=[0,0,0,0];
-  var f=v>>16,w,c,i,s,t;
-  if(f){
-    for(i=0;i<4;++i){
-      if(v&(1<<i))w=i;
-      if(v&(1<<(i+4)))c=i;
+function calcRS(v, b) {
+  const a = [0, 0, 0, 0], f = v >> 16
+  let w, c, i, s, t
+  if (f) {
+    for (i = 0; i < 4; ++i) {
+      if (v & (1 << i))w = i
+      if (v & (1 << (i + 4)))c = i
     }
-    if(w!==c){
-      s=-b;
-      for(i=0;i<4;++i){
-        if(i===w)a[i]=f+b*3;
-        else if(i===c)a[i]=s-f;
-        else a[i]=s;
+    if (w !== c) {
+      s = -b
+      for (i = 0; i < 4; ++i) {
+        if (i === w)a[i] = f + b * 3
+        else if (i === c)a[i] = s - f
+        else a[i] = s
+      }
+    } else {
+      s = b + f
+      t = s * 3
+      s = -s
+      for (i = 0; i < 4; ++i) {
+        a[i] = i !== w ? s : t
       }
     }
-    else{
-      s=b+f;
-      t=s*3;
-      s=-s;
-      for(i=0;i<4;++i){
-        a[i]=i!==w?s:t;
-      }
-    }
   }
-  return a;
+  return a
 }
 
-function calc_rp(v,f){
-  var a=[0,0,0,0];
-  var i,c=0,t;
-  for(i=0;i<4;++i){
-    if(v&(1<<(i+8))){
-      a[i]-=40;
-      ++c;
+function calcRP(v, f) {
+  const a = [0, 0, 0, 0]
+  let i, c = 0, t
+  for (i = 0; i < 4; ++i) {
+    if (v & (1 << (i + 8))) {
+      a[i] -= 40
+      ++c
     }
   }
-  if(c&&f){
-    t=c*10;
-    for(i=0;i<4;++i){
-      a[i]+=t;
+  if (c && f) {
+    t = c * 10
+    for (i = 0; i < 4; ++i) {
+      a[i] += t
     }
   }
-  return a;
+  return a
 }
 
 export function fillDocumentRounds(doc: any): any {
-  let rounds = []
-  const bs = doc.g.b == undefined ? 8 : doc.g.b
+  const rounds = []
+  const bs = doc.g.b === undefined ? 8 : doc.g.b
   const fa = doc.g.fa
-  for (let rd of doc.rd) {
+  for (const rd of doc.rd) {
     rounds.push({
-      rs: calc_rs(rd.rbf, bs),
-      rp: calc_rp(rd.rbf, fa)
+      rs: calcRS(rd.rbf, bs),
+      rp: calcRP(rd.rbf, fa),
     })
   }
   doc.rounds = rounds
